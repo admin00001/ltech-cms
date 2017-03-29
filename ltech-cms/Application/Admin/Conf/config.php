1c173ed9deb80011f67dcd340672b47facc57315
admin00001 <1412490824@qq.com>
2017-03-29 16:22:27 +0800

    remove ltech-cms

--- ltech-cms/Application/Admin/Conf/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-    'DEFAULT_CONTROLLER' => 'Login', // 默认控制器名称
-   
-);
--- ltech-cms/Application/Home/Conf/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-    'DEFAULT_CONTROLLER' => 'Index', // 默认控制器名称
-);
